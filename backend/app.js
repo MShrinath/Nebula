f87ec004f53c6940f321d90821c5ec6d3cd7cbd3
MShrinath <112850938+MShrinath@users.noreply.github.com>
2025-05-02 01:23:32 +0530

    Try hardcoding

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 const bcrypt = require('bcrypt');
 
-const dbPath = path.join(__dirname, 'database.sqlite');
+const dbPath = path.resolve('database.sqlite');
 const db = new sqlite3.Database(dbPath);
 
 exports.handler = async function(event, context) {
